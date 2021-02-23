f8fbfb1eef23098f4da0003610e27823d0a0a5c0
mh <mh@immerda.ch>
2021-02-23 14:48:27 +0100

    disable managing of resolv.conf through NM

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,22 +13,37 @@
 # the Free Software Foundation.
 #
 
-class resolvconf(
-  String $domain = $facts['domain'],
-  String $search = $facts['domain'],
-  Array[Stdlib::IP::Address::Nosubnet,1]
-    $nameservers = [ '8.8.8.8' ],
-  Optional[String]
-    $tail        = undef,
+class resolvconf (
+  String $domain = $facts['networking']['domain'],
+  String $search = $facts['networking']['domain'],
+  Array[Stdlib::IP::Address::Nosubnet,1] $nameservers = ['8.8.8.8'],
+  Optional[String] $tail = undef,
 ) {
-  $content = $facts['operatingsystem'] ? {
-    "OpenBSD" => template("resolvconf/resolvconf.${facts['operatingsystem']}.erb"),
+  $content = $facts['os']['name'] ? {
+    'OpenBSD' => template("resolvconf/resolvconf.${facts['os']['name']}.erb"),
     default   => template('resolvconf/resolvconf.erb'),
   }
-  file{'/etc/resolv.conf':
+  file { '/etc/resolv.conf':
     content => $content,
     owner   => root,
     group   => 0,
     mode    => '0444';
+  }
+  # disable dns management via NetworkManager
+  if ($facts['os']['name'] == 'CentOS') and versioncmp($facts['os']['release']['major'],'8') >= 0 {
+    file { '/etc/NetworkManager/conf.d/90-dns-none.conf':
+      content => "[main]\ndns=none\n",
+      owner   => root,
+      group   => 0,
+      mode    => '0644',
+      notify  => Service['NetworkManager'],
+    }
+    if !defined(Service['NetworkManager']) {
+      service { 'NetworkManager':
+        ensure    => running,
+        hasstatus => true,
+        enable    => true,
+      }
+    }
   }
 }
